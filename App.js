f14e58960600a361ce45619da370ec718e1658ed
SungDongHwan <himmysky@pusan.ac.kr>
2023-02-24 09:19:06 +0900

    "ver 1.0"

---
+++ App.js
@@ -0,0 +1,25 @@
+//import './App.css';
+//import Gal from './06/Gal'
+import { BrowserRouter } from 'react-router-dom';
+import Routermain from './09/Routermain';
+import Wmain from './10/Wmain';
+import Boxoffice from './11/Boxoffice'
+const App=()=> {
+  return (
+  //  <div>
+  //    <Frcst />
+  //  </div>
+  //<Taccident/>
+  //  <Mycom />
+   //<Gal/>
+   //<Myform/>
+   //<GalSelect/>
+   //<BrowserRouter>
+   // <Wmain/>
+   //</BrowserRouter>
+   <Boxoffice/>
+   
+      );
+}
+
+export default App;
